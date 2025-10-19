b88d574757bc5692f2d8d5ce844891ff40adc722
Chapss <palmakarsten@gmail.com>
2025-10-19 18:34:31 +0800

    Objective Complete: NO bugs/errors

--- src/STDL/TDL.java
+++ src/STDL/TDL.java
@@ -9,11 +9,12 @@ public class TDL {
 		Scanner input = new Scanner(System.in);
 		ArrayList<String> userTDAL = new ArrayList<String>();
 		// initialize variables
-		String userATD, userRetry;
-		int uRTD, uStart;
+		String userADD, userRetry, uSearch;
+		int uStart;
 		
 		
 		// using do while to repeat the code
+		outer_layer:
 		do {
 			tdlIntro();
 			// instructions
@@ -21,61 +22,67 @@ public class TDL {
 			uStart = input.nextInt();
 			// use switch for better layout
 			switch(uStart) {
-			// case 1 - let the user know he/she saved something in to-do list
+			// case 1 - adding a product
 				case 1:
-					PTDL(userTDAL);
-					break;
-			// case 2 - adding a to-do
-				case 2:
 					while (true) {
-						// list of to-do 
+						// list of product
 						PTDL(userTDAL);
 						// instructions
-						System.out.printf("%nInput your to-do here (input 'exit' to stop): ");
-						userATD = input.nextLine();
-						// To prevent empty input from the user
-						if (userATD.isEmpty()) {
-							System.out.printf("%nPlease provide a to-do!%n%n");
-						}
-						// To exit inputing to-do
-						if (userATD.equalsIgnoreCase("exit")) break;
+						System.out.printf("%nInput your products here (input 'exit' to stop): ");
+						userADD = input.nextLine();
+						
+						// To exit inputing products
+						if (userADD.equalsIgnoreCase("exit")) break;
 						// remove the spaces entry
 						userTDAL.removeIf(inputs -> inputs.trim().isEmpty());
 						// adding the user input into array list named userTDAL
-						userTDAL.add(userATD);
+						userTDAL.add(userADD);
 					}
 					// SAVED ONLY WHEN LOOPING "Yes" (RESET IF QUITTING "No"/FRESH START)
-					System.out.printf("%nTo-do list saved!%n");
+					System.out.printf("%nProduct/s are saved!%n");
 					break;
-			// case 3 - delete a to-do
+					
+			// case 2 - let the user know he/she saved something in product list
+				case 2:
+					PTDL(userTDAL);
+					break;
+					
+			// case 3 - search a products
 				case 3:
 					while(true) {
-						// list of to-do 
-						PTDL(userTDAL);
 						// instructions
-						System.out.printf("%nIf you want to delete a to-do, choose from the numbers.");
-						System.out.printf("%nInput '0' if nothing: ");
-						uRTD = input.nextInt();
-						// if user type 0 it will break the while loop
-						if ((uRTD) == 0) break;
-						// to remove the to-do based on their index position
-						userTDAL.remove(uRTD-1);
+						System.out.printf("%n%nIf you want to search a product input their names.");
+						System.out.printf("%nInput 'exit' if nothing/stop: ");
+						uSearch = input.nextLine();
+						
+						// they want to exit
+						if (uSearch.equalsIgnoreCase("exit")) break;
+						
+						// search logic using "contains" attribute
+						if (userTDAL.contains(uSearch)) {
+							System.out.printf("%nThe product %s is listed!", uSearch);
+						}
+						
+						// if not yet listed
+						if (!userTDAL.contains(uSearch)) {
+							System.out.printf("%nThe product %s is not listed!", uSearch);						}
 					}
-					// SAVED ONLY WHEN LOOPING "Yes" (RESET IF QUITTING "No"/FRESH START)
-					System.out.printf("%nTo-do list saved!%n");
 					break;
+					
 			// case 4 - Exiting the from the choices
 				case 4:
-					System.out.println("Exiting...");
-					break;
-			// default - when the user input beyond (1-3)
+					System.out.printf("%n%nExiting...");
+					// exit quickly
+					break outer_layer;
+					
+			// default - when the user input beyond (1-4)
 				default:
 					System.out.println("Invalid option");
 					
 			}
 			// to ask the user if they want to try again the program
 			while (true) {
-				System.out.printf("%nDo you want to try again? (Yes/No): ");
+				System.out.printf("%nBack to Menu? (Yes/No): ");
 				userRetry = input.next();
 				// if the user don't choose from (Yes/No)
 				if (!userRetry.equalsIgnoreCase("Yes") && !userRetry.equalsIgnoreCase("No")) {
@@ -92,20 +99,21 @@ public class TDL {
 	}
 	// introduction/guide
 	public static void tdlIntro() {
-		System.out.printf("%nWelcome to Simple To-Do List Program!%n%n");
+		System.out.printf("%nWelcome to Inventory System!%n%n");
 		System.out.println("Choose from the options!");
-		System.out.println("1. To-Do List");
-		System.out.println("2. Add a To-Do");
-		System.out.println("3. Remove a To-Do");
+		System.out.println("1. Add a Product");
+		System.out.println("2. Products List");
+		System.out.println("3. Search Products");
 		System.out.println("4. Exit");
 	}
 	
 	// printing the output
 	public static void PTDL(ArrayList<String> userTDAL) {
-		System.out.printf("%nTO-DO LIST:%n");
+		System.out.printf("%nPRODUCT/S LIST:%n");
 		// print the object inside of userTDAL Array List
 		for (int i=0; i<userTDAL.size(); i++) {
 			System.out.println((i+1) + ". " + userTDAL.get(i));
 		}
 	}
+	
 }
